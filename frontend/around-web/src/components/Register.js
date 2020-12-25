65b469986d540d2f9cc8b420cd552cc0fb2e4dd1
derek <wl328@njit.edu>
2020-12-25 11:50:16 -0500

    show the photo and video on web

--- frontend/around-web/src/components/Register.js
+++ frontend/around-web/src/components/Register.js
@@ -55,7 +55,7 @@ function Register(props) {
             })
             .catch( error => {
                 console.log('register failed: ', error.message);
-                message.success('Registration failed!');
+                message.error('Registration failed!');
             })
     };
 
--- frontend/around-web/src/constants.js
+++ frontend/around-web/src/constants.js
@@ -1,6 +1,6 @@
 export const TOKEN_KEY = 'token';
-export const BASE_URL = 'http://around-295701.ue.r.appspot.com';
-
+export const BASE_URL = 'https://around-295701.ue.r.appspot.com';
+// export const BASE_URL = "https://around-dot-around-179500.uc.r.appspot.com";
 export const SEARCH_KEY = {
     all: 0,
     keyword: 1,
